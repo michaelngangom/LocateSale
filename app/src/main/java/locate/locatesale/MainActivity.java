e69c217023592b63274b6e5cda78d0e8e593e827
Michael Ngangom <michael.ngangom@gmail.com>
2016-12-14 22:02:09 +0100

    new activity

--- app/src/main/java/locate/locatesale/MainActivity.java
+++ app/src/main/java/locate/locatesale/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
 
+import static android.R.attr.value;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     public void openSetting(View view)
     {
         Intent intent = new Intent(this, SettingActivity.class);
+        intent.putExtra("key",value);
+        this.startActivity(intent);
 
     }
 }
--- app/src/main/java/locate/locatesale/SettingActivity.java
+++ app/src/main/java/locate/locatesale/SettingActivity.java
@@ -1,5 +1,6 @@
 package locate.locatesale;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        Intent intent = getIntent();
+
     }
 }
